493310eb2533dbb375da426fe7f21903b7f1e88a
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-03-09 20:11:37 +0530

    Update SinglePageDummy.jsx

--- zoosafari/src/Component/DummyData/SinglePageDummy.jsx
+++ zoosafari/src/Component/DummyData/SinglePageDummy.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Dummydata } from './Dummydata';
 import { useParams } from "react-router-dom";
 import '../Singlepage/Animate.css'
